076b44d69ac8522611dcb9d94444d3fdf8f11ecb
Igor <pa220282kiv@gmail.com>
2025-01-16 12:16:55 +0200

    Add lazy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation/Navigation';
 import HomePage from '../pages/HomePage/HomePage';
-import MoviesPage from '../pages/MoviesPage/MoviesPage';
+const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
 import NotFoundPage from '../pages/NotFoundPage/NotFoundPage';
-import MovieDetailsPage from '../pages/MovieDetailsPage/MovieDetailsPage';
-import MovieCast from './MovieCast/MovieCast';
-import MovieReviews from './MovieReviews/MovieReviews';
+const MovieDetailsPage = lazy(() =>
+  import('../pages/MovieDetailsPage/MovieDetailsPage')
+);
+const MovieCast = lazy(() => import('./MovieCast/MovieCast'));
+const MovieReviews = lazy(() => import('./MovieReviews/MovieReviews'));
 
 function App() {
   return (
